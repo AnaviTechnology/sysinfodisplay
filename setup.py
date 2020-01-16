f6933a7353b2244cc0d723eda2b93b0cc2789fb1
Leon Anavi <leon@anavi.org>
2020-01-16 19:34:16 +0200

    setup.py: Add setup.py
    Signed-off-by: Leon Anavi <leon@anavi.org>

---
+++ setup.py
@@ -0,0 +1,13 @@
+import sys
+from setuptools import setup
+setup(
+    name = "sysinfodisplay",
+    version = "0.0.1",
+    packages=["sysinfodisplay"],
+    author="Leon Anavi",
+    author_email = "leon@anavi.org",
+    description = "Display system information like IP and status of systemd services on mini I2C OLED display",
+    license = "GPLv3",
+    keywords= "system",
+    url = "https://github.com/AnaviTechnology/sysinfodisplay",
+)
